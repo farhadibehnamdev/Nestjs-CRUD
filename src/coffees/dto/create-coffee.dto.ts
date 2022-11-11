a1eed7adeab5e224cec243c6e4a034ce76c9b7b2
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-11-11 10:41:53 +0330

    chore:Added decorator type for each property in dtos.

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,5 +1,10 @@
+import { IsString } from 'class-validator';
+
 export class CreateCoffeeDto {
+  @IsString()
   name: string;
+  @IsString()
   brand: string;
+  @IsString({ each: true })
   flavors: string[];
 }
--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,5 +1,4 @@
-export class UpdateCoffeeDto {
-  name?: string;
-  brand?: string;
-  flavors?: string[];
-}
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCoffeeDto } from './create-coffee.dto';
+
+export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
