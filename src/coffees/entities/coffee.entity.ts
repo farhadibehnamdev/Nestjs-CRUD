1e91ea222557df95114700fdd4113075cd5c927e
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-11-11 18:52:05 +0330

    ♻️ refactor:Added many to many relationship between entities.

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,23 @@
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  JoinTable,
+  ManyToMany,
+} from 'typeorm';
+import { Flavor } from './flavor.entity';
+
+@Entity()
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+  @Column()
   brand: string;
+
+  @JoinTable()
+  @ManyToMany((type) => Flavor, (flavor) => flavor.coffees)
   flavors: string[];
 }
