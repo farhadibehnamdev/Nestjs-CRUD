e68010c4b5bc8dc4b78b9c5c9d6efb2622786bcb
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-11-12 14:53:29 +0330

    🔧 chore:Imlemented pagination.

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -5,10 +5,12 @@ import {
   Param,
   Patch,
   Post,
+  Query,
   Delete,
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { PaginationQueryDto } from '../common/pagination-query-dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
 @Controller('coffees')
@@ -16,8 +18,8 @@ export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
   @Get()
-  findAll() {
-    return this.coffeesService.findAll();
+  findAll(@Query() paginationQuery: PaginationQueryDto) {
+    return this.coffeesService.findAll(paginationQuery);
   }
 
   @Get(':id')
